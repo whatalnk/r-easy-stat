1ce19a391fb07e2e67252a910403e5ff0e0d82e3
Wataru Nakai <watnk0806@gmail.com>
2016-05-20 02:10:35 +0900

    HTML の出力先を設定

--- jupyter_nbconvert_config.py
+++ jupyter_nbconvert_config.py
@@ -3,3 +3,4 @@ c = get_config()
 c.Exporter.preprocessors = ['nbconvert.preprocessors.ExtractOutputPreprocessor']
 c.NbConvertApp.export_format = 'html'
 c.Exporter.template_file = 'separate_image_html'
+c.FilesWriter.build_directory = 'gh-pages'
