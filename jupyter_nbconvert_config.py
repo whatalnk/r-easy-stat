48bfa1574e2bf6080fae2fa85e4688765f75d2b5
Wataru Nakai <watnk0806@gmail.com>
2016-05-20 02:51:17 +0900

    画像を svg+xml に限定（png を無視）

--- jupyter_nbconvert_config.py
+++ jupyter_nbconvert_config.py
@@ -1,6 +1,7 @@
 c = get_config()
 
 c.Exporter.preprocessors = ['nbconvert.preprocessors.ExtractOutputPreprocessor']
+c.ExtractOutputPreprocessor.extract_output_types = {'image/svg+xml'}
 c.NbConvertApp.export_format = 'html'
 c.Exporter.template_file = 'separate_image_html'
 c.FilesWriter.build_directory = 'gh-pages'
